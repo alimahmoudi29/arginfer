02e0956b6c215d371d0e0e64b58933c9ef3ab561
Ali Mahmoudi <amahmoudi@student.unimelb.edu.au>
2021-03-17 21:10:34 +1100

    make html

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev61+g783d642.d20210317',
+    VERSION: '0.1.dev62+gc5f993c.d20210317',
     LANGUAGE: 'English',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
